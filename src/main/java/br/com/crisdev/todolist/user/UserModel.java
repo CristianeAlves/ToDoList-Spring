12e1e808ea44e8a76e6477e1ccd3ccfac16857ab
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-10-09 18:25:56 -0300

    Commit inicial

---
+++ src/main/java/br/com/crisdev/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.crisdev.todolist.user;
+
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+}
